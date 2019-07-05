52e031e076624cbe5535c7cf66197bf9d6db2245
fanfanfufu <1127137365@qq.com>
2019-07-05 23:55:19 +0800

    0705买卖股票问题添加

--- src/main/java/com/fuqi/month06/day0620/BestTimetoBuyandSellStockwithCooldown.java
+++ src/main/java/com/fuqi/month06/day0620/BestTimetoBuyandSellStockwithCooldown.java
@@ -9,7 +9,8 @@ package com.fuqi.month06.day0620;
 public class BestTimetoBuyandSellStockwithCooldown {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        if (n <= 0){ return 0; }
+        if (n <= 1){ return 0; }
+        if (n == 2){ return prices[1] - prices[0] > 0 ? prices[1] - prices[0] : 0; }
 
         int[][] dp = new int[n][2];
         dp[0][1] = -prices[0];
@@ -17,9 +18,14 @@ public class BestTimetoBuyandSellStockwithCooldown {
         dp[1][1] = Math.max(dp[0][1], dp[0][0] - prices[1]);
 
         for (int i = 2; i < n; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][0]);
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-2][0] - prices[i]);
         }
 
         return dp[n-1][0];
+    }
+
+    public static void main(String[] args) {
+
     }
 }
