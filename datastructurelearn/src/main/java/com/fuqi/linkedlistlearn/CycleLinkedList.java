cf9a0d56c4f909fc6984461e3010305ea88dbb6f
fanfanfufu <1127137365@qq.com>
2019-07-19 15:04:11 +0800

    0719提交

--- datastructurelearn/src/main/java/com/fuqi/linkedlistlearn/CycleLinkedList.java
+++ datastructurelearn/src/main/java/com/fuqi/linkedlistlearn/CycleLinkedList.java
@@ -43,7 +43,7 @@ public class CycleLinkedList {
      * 方法二：floyd法
      * 时间复杂度O(n)；空间复杂度O(1)
      * @param head
-     * @return
+     * @return ListNode
      */
     public ListNode detectCycle2(ListNode head){
         if (head == null || head.next == null){ return null; }
