7e94587449b83412f1d215e20a1b05e6ef4229d3
fanfanfufu <1127137365@qq.com>
2019-08-06 16:12:37 +0800

    0806每日一题

--- src/main/java/com/fuqi/month08/theswordreferstooffer/Solution01.java
+++ src/main/java/com/fuqi/month08/theswordreferstooffer/Solution01.java
@@ -8,85 +8,73 @@ package com.fuqi.month08.theswordreferstooffer;
  * @Version V1.0
  */
 public class Solution01 {
-    public boolean find(int target, int [][] array) {
+    /**
+     * 方法一：逐行二分查找
+     * 时间复杂度 O(nlogn)
+     * @param target
+     * @param array
+     * @return Boolean
+     */
+    public boolean find1(int target, int [][] array) {
         if (array == null || array.length == 0 || array[0].length == 0){
             return false;
         }
 
-        int[] firstCol = new int[array.length];
-
-        // 取出第一列
         for (int i = 0; i < array.length; i++) {
-            firstCol[i] = array[i][0];
-        }
-
-        int left = 0;
-        int right = firstCol.length-1;
-        int mid = 0;
-
-        if (firstCol[left] > target){
-            return false;
-        }
-
-        if (firstCol[right] < target){
-            for (int i = array.length-1; i >= 0; i--) {
-                for (int j = array[i].length-1; j >= 0; j--) {
-                    if (array[i][j] == target){
-                        return true;
-                    }
+            int low = 0;
+            int high = array[i].length - 1;
+            int middle = 0;
+            while (low <= high){
+                middle = (low + high) / 2;
+                if (array[i][middle] > target){
+                    high = middle - 1;
+                }else if (array[i][middle] < target){
+                    low = middle + 1;
+                }else {
+                    return true;
                 }
             }
+        }
+
+        return false;
+
+    }
+
+    /**
+     * 方法二：从左上角或者右下角，这里选择从右下角开始搜索
+     * 若目标大于当前数，则往下搜索，行数+1
+     * 若目标小于当前数，则往左搜索，列数-1
+     * 直到越界，没有搜索到则返回false
+     * @param target
+     * @param array
+     * @return boolean
+     */
+    public static boolean find2(int target, int[][] array){
+        if (array == null || array.length == 0 || array[0].length == 0){
             return false;
         }
 
-        while (left <= right){
-            mid = (left + right) / 2;
-            if (firstCol[mid] > target){
-                if (firstCol[mid-1] < target){
-                    return (commonBinarySearch(array[mid-1], target) != -1);
-                }
-                right = mid - 1;
-            }else if (firstCol[mid] < target){
-                if (firstCol[mid+1] > target){
-                    return (commonBinarySearch(array[mid+1], target) != -1);
-                }
-                left = mid + 1;
-            }else {
+        int row = 0;
+        int col = array[0].length - 1;
+        while (row < array.length && col >= 0){
+            if (target == array[row][col]){
                 return true;
+            }else if (target > array[row][col]){
+                row++;
+            }else {
+                col--;
             }
         }
 
         return false;
     }
 
-    public static int commonBinarySearch(int[] arr, int target){
-        int low = 0;
-        int high = arr.length - 1;
-        int middle = 0;
-
-        if (arr[low] > target || target > arr[high]) {
-            return -1;
-        }
-
-        while (low <= high){
-            middle = (low + high) / 2;
-            if (arr[middle] > target){
-                high = middle - 1;
-            }else if (arr[middle] < target){
-                low = middle + 1;
-            }else {
-                return middle;
-            }
-        }
-
-        return -1;
-    }
-
     public static void main(String[] args) {
         int[][] array = {{1,2,8,9}, {2,4,9,12}, {4,7,10,13}, {6,8,11,15}};
         int target = 7;
         Solution01 solution01 = new Solution01();
-        boolean res = solution01.find(target, array);
+//        boolean res = solution01.find1(target, array);
+        boolean res = find2(target, array);
         System.out.println("res = " + res);
     }
 }
