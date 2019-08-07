4cda40bc0a50cf6d6424aacb9de4b971f0ad788d
fanfanfufu <1127137365@qq.com>
2019-08-07 16:30:50 +0800

    0807每日一题

---
+++ src/main/java/com/fuqi/month08/theswordreferstooffer/ListNode.java
@@ -0,0 +1,16 @@
+package com.fuqi.month08.theswordreferstooffer;
+
+/**
+ * @Description: 单链表的简单定义
+ * @Author 傅琦
+ * @date 2019/8/7 10:04
+ * @Version V1.0
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x){
+        val = x;
+    }
+}
