4233d8306682044510b73775c85b4522916c438a
fanfanfufu <1127137365@qq.com>
2019-08-09 11:29:41 +0800

    0809每日练习

--- src/main/java/com/fuqi/month08/theswordreferstooffer/Solution13.java
+++ src/main/java/com/fuqi/month08/theswordreferstooffer/Solution13.java
@@ -1,7 +1,7 @@
 package com.fuqi.month08.theswordreferstooffer;
 
 /**
- * @Description:
+ * @Description: 使奇数位于偶数前面，并保证相对顺序不变
  * @Author 傅琦
  * @date 2019/8/8 11:26
  * @Version V1.0
@@ -15,7 +15,9 @@ public class Solution13 {
         for (int i = 0; i < n; i++) {
             // 扫描到当前数是奇数
             if (array[i] % 2 == 1){
+                // 记录下当前奇数的位置
                 int j = i;
+                // 将当前的奇数往左依次挪动
                 while (j > k){
                     int temp = array[j];
                     array[j] = array[j-1];
