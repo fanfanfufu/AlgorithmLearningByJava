b58f56291ebe094ba58e3737785776eefface3e7
fanfanfufu <1127137365@qq.com>
2019-08-19 21:59:44 +0800

    0819商汤

---
+++ src/main/java/com/fuqi/month08/shangtang/Solution1.java
@@ -0,0 +1,54 @@
+package com.fuqi.month08.shangtang;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * @Description:
+ * @Author 傅琦
+ * @date 2019/8/19 19:44
+ * @Version V1.0
+ */
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String[] input = s.split(" ");
+        for (String str : input) {
+            System.out.print(str + " ");
+        }
+
+        System.out.println(helper(input));
+    }
+
+    private static int helper(String[] symbols) {
+        Deque<Integer> numbers = new LinkedList<>();
+        for (String string : symbols) {
+            if ("+".equals(string)){
+                int right = numbers.pop();
+                int left = numbers.pop();
+                int res = left + right;
+                numbers.push(res);
+            }else if ("-".equals(string)){
+                int right = numbers.pop();
+                int left = numbers.pop();
+                int res = left - right;
+                numbers.push(res);
+            }else if ("*".equals(string)){
+                int right = numbers.pop();
+                int left = numbers.pop();
+                int res = left * right;
+                numbers.push(res);
+            }else if ("/".equals(string)){
+                int right = numbers.pop();
+                int left = numbers.pop();
+                int res = left / right;
+                numbers.push(res);
+            }else {
+                numbers.push(Integer.parseInt(string));
+            }
+        }
+        return numbers.pop();
+    }
+}
---
+++ src/main/java/com/fuqi/month08/shangtang/Solution2.java
@@ -0,0 +1,45 @@
+package com.fuqi.month08.shangtang;
+
+import java.util.Scanner;
+
+/**
+ * @Description:
+ * @Author 傅琦
+ * @date 2019/8/19 20:02
+ * @Version V1.0
+ */
+public class Solution2 {
+    private static int helper(int[] arr, int location) {
+        int len = 1;
+        int i = location-1, j = location+1;
+        int judge = arr[location];
+        while (i >= 0 && j < arr.length && arr[i] < judge && judge > arr[j]) {
+            len += 2;
+            i--;
+            j++;
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int len = scanner.nextInt();
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int ans = 0;
+        for (int i = 0; i < len; i++) {
+            int temp = helper(arr, i);
+            ans = ans > temp ? ans : temp;
+        }
+
+        if (ans == 1){
+            System.out.println(0);
+        }else {
+            System.out.println(ans);
+        }
+        scanner.close();
+    }
+}
