9116a8b45785f9f2534fe77d2b1a77dc524f761b
fanfanfufu <1127137365@qq.com>
2019-09-04 11:26:17 +0800

    0904日常

--- datastructurelearn/src/main/java/com/fuqi/treelearn/AfterOrderIterator.java
+++ datastructurelearn/src/main/java/com/fuqi/treelearn/AfterOrderIterator.java
@@ -1,6 +1,8 @@
 package com.fuqi.treelearn;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -30,5 +32,50 @@ public class AfterOrderIterator {
         res.add(root.val);
 
         return res;
+    }
+
+    /**
+     * 后序遍历的非递归算法比较麻烦，跟之前有所不同，
+     * 需要一个额外的指针pre指向上次打印的节点，如果一个节点没有左右孩子，那么打印，
+     * 如果一个节点的左右孩子都已经打印过，那么这个节点也可以打印。
+     * 而新加的pre指针就是用来判断左右孩子有没有被打印过的。
+     *
+     * 对于任一节点P，
+     *
+     * 1）先将节点P入栈；
+     *
+     * 2）若P不存在左孩子和右孩子，或者P存在左孩子或右孩子，但左右孩子已经被输出，则可以直接输出节点P，
+     *    并将其出栈，将出栈节点P标记为上一个输出的节点，再将此时的栈顶结点设为当前节点；
+     *
+     * 3）若不满足2）中的条件，则将P的右孩子和左孩子依次入栈，当前节点重新置为栈顶结点，之后重复操作2）；
+     *
+     * 4）直到栈空，遍历结束。
+     *
+     */
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) { return ans; }
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        TreeNode cur;
+        TreeNode pre = null;
+
+        while (!stack.isEmpty()) {
+            cur = stack.peek();
+            if ((cur.left == null && cur.right == null) || (pre != null && (cur.left == pre || cur.right == pre))) {
+                pre = stack.pop();
+                ans.add(pre.val);
+            } else {
+                if (cur.right != null) {
+                    stack.push(cur.right);
+                }
+                if (cur.left != null) {
+                    stack.push(cur.left);
+                }
+            }
+        }
+
+        return ans;
     }
 }
--- datastructurelearn/src/main/java/com/fuqi/treelearn/MidOrderIterator.java
+++ datastructurelearn/src/main/java/com/fuqi/treelearn/MidOrderIterator.java
@@ -1,6 +1,8 @@
 package com.fuqi.treelearn;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -34,5 +36,44 @@ public class MidOrderIterator {
         }
 
         return res;
+    }
+
+    /**
+     * 中序遍历的非递归实现
+     *
+     * 对于任一节点P，
+     *
+     * 1）若P的左孩子不为空，则将P入栈并将P的左孩子置为当前节点，然后再对当前节点进行相同的处理；
+     *
+     * 2）若P的左孩子为空，则输出P节点，而后将P的右孩子置为当前节点，看其是否为空；
+     *
+     * 3）若不为空，则重复1）和2）的操作；
+     *
+     * 4）若为空，则执行出栈操作，输出栈顶节点，并将出栈的节点的右孩子置为当前节点，看起是否为空，重复3）和4）的操作；
+     *
+     * 5）直到当前节点P为NULL并且栈为空，则遍历结束。
+     */
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) { return ans; }
+
+        Deque<TreeNode> stack = new LinkedList<>();
+
+        TreeNode cur = root;
+
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }else {
+                while (cur == null && !stack.isEmpty()) {
+                    TreeNode temp = stack.pop();
+                    ans.add(temp.val);
+                    cur = temp.right;
+                }
+            }
+        }
+
+        return ans;
     }
 }
