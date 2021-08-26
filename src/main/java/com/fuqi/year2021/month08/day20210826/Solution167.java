8960d26d6aa962289dfdc9f578fe8aa42a72e5d4
fanfanfufu <1127137365@qq.com>
2021-08-27 00:28:39 +0800

    2021-08-26：算法入门练习第3天-2

--- src/main/java/com/fuqi/year2021/month08/day20210826/Solution167.java
+++ src/main/java/com/fuqi/year2021/month08/day20210826/Solution167.java
@@ -5,7 +5,7 @@ package com.fuqi.year2021.month08.day20210826;
  */
 public class Solution167 {
     /**
-     * 暴力解法
+     * 暴力解法：O(n^n)
      *
      * @param numbers
      * @param target
@@ -26,6 +26,69 @@ public class Solution167 {
                 break;
             }
             i++;
+        }
+
+        return ans;
+    }
+
+    /**
+     * 优化:  二分查找 O(nlogn)
+     *
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] numbers, int target) {
+        int[] ans = new int[2];
+        for (int i = 0; i < numbers.length; i++) {
+            int another = target - numbers[i];
+            int left = i+1, right = numbers.length;
+            int j = findAnother(left, right, numbers, another);
+            if (j != 0) {
+                ans[0] = i + 1;
+                ans[1] = j + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private int findAnother(int left, int right, int[] nums, int target) {
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] > target) {
+                right = mid;
+                continue;
+            }
+            left = mid + 1;
+        }
+        return 0;
+    }
+
+    /**
+     * 再优化：双指针O(n)
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public int[] twoSum3(int[] numbers, int target) {
+        int[] ans = new int[2];
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) {
+                ans[0] = i+1;
+                ans[1] = j+1;
+                return ans;
+            }
+            if (sum < target) {
+                i++;
+                continue;
+            }
+            j--;
         }
 
         return ans;
