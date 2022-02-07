c331e7d314dd3407be61de9f1f784760577ee9dd
fanfanfufu <1127137365@qq.com>
2022-02-07 23:43:58 +0800

    2022-02-07：每日练习

---
+++ src/main/java/com/fuqi/year2022/month02/Solution00034.java
@@ -0,0 +1,33 @@
+package com.fuqi.year2022.month02;
+
+/**
+ * @author FuQi
+ * @date 2022/2/7 23:18
+ * @description
+ */
+public class Solution00034 {
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false)-1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx]== target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    private int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length-1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid-1;
+                ans = mid;
+            } else {
+                left = mid+1;
+            }
+        }
+
+        return ans;
+    }
+}
