0b196a86cb89bcfc0dd61ecd3acfdca44c63f45e
fanfanfufu <1127137365@qq.com>
2022-02-16 23:22:17 +0800

    2022-02-16：每日练习

---
+++ src/main/java/com/fuqi/year2022/month02/Solution00040.java
@@ -0,0 +1,49 @@
+package com.fuqi.year2022.month02;
+
+import java.util.*;
+
+/**
+ * @author FuQi
+ * @date 2022/2/16 22:47
+ * @description
+ */
+public class Solution00040 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        int len = candidates.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+
+        // 关键步骤
+        Arrays.sort(candidates);
+
+        Deque<Integer> path = new ArrayDeque<>(len);
+        dfs(candidates, len, 0, target, path, res);
+
+        return res;
+    }
+
+    private void dfs(int[] candidates, int len, int begin, int target, Deque<Integer> path, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = begin; i < len; i++) {
+            if (target - candidates[i] < 0) {
+                break;
+            }
+
+            if (i > begin && candidates[i-1] == candidates[i]) {
+                // 去重
+                continue;
+            }
+            path.offerLast(candidates[i]);
+
+            dfs(candidates, len, i+1, target-candidates[i], path, res);
+
+            path.pollLast();
+        }
+    }
+}
---
+++ src/main/java/com/fuqi/year2022/month02/Solution00047.java
@@ -0,0 +1,43 @@
+package com.fuqi.year2022.month02;
+
+import java.util.*;
+
+/**
+ * @author FuQi
+ * @date 2022/2/16 22:15
+ * @description
+ */
+public class Solution00047 {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        if (nums.length == 0) {
+            return new ArrayList<>();
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        Deque<Integer> path = new LinkedList<>();
+        boolean[] used = new boolean[nums.length];
+        Arrays.sort(nums);
+
+        dfs(nums, 0, path, ans, used);
+
+        return ans;
+    }
+
+    private void dfs(int[] nums, int depth, Deque<Integer> path, List<List<Integer>> ans, boolean[] used) {
+        if (depth == nums.length) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i] || (i > 0 && nums[i] == nums[i-1] && !used[i-1])) {
+                continue;
+            }
+            path.add(nums[i]);
+            used[i] = true;
+            dfs(nums, depth+1, path, ans, used);
+            used[i] = false;
+            path.pollLast();
+        }
+    }
+}
