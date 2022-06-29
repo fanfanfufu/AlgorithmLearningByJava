55cfb62b0d256b591c11b7e377e356579cf637ab
fanfanfufu <1127137365@qq.com>
2022-06-29 23:27:35 +0800

    2022-06-29：每日练习

--- src/main/java/com/fuqi/year2022/month02/Solution00022.java
+++ src/main/java/com/fuqi/year2022/month02/Solution00022.java
@@ -6,17 +6,17 @@ import java.util.List;
 /**
  * @author FuQi
  * @date 2022/2/17 22:53
- * @description
+ * @description 括号生成
  */
 public class Solution00022 {
-    public List<String> generateParenthesis(int n) {
+    public static List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
         backtrack(ans, new StringBuilder(), 0, 0, n);
 
         return ans;
     }
 
-    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+    public static void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
         if (cur.length() == max * 2) {
             ans.add(cur.toString());
             return;
@@ -26,10 +26,15 @@ public class Solution00022 {
             backtrack(ans, cur, open + 1, close, max);
             cur.deleteCharAt(cur.length() - 1);
         }
+        // close < open，能保证左右括号数量相等，不会出现一些非法的组合
         if (close < open) {
             cur.append(')');
             backtrack(ans, cur, open, close + 1, max);
             cur.deleteCharAt(cur.length() - 1);
         }
+    }
+
+    public static void main(String[] args) {
+        generateParenthesis(3);
     }
 }
