9625e3bc975c1a1a9adceff9fa50f427df66caac
fanfanfufu <1127137365@qq.com>
2022-08-25 23:22:59 +0800

    2022-08-25：每日一练
    240：搜索二维矩阵||

---
+++ src/main/java/com/fuqi/year2022/month08/Solution00240.java
@@ -0,0 +1,83 @@
+package com.fuqi.year2022.month08;
+
+/**
+ * @author FuQi
+ * @date 2022/8/25 21:48
+ * @description 搜索二维矩阵||
+ */
+public class Solution00240 {
+    /**
+     * 暴力法
+     *
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix1(int[][] matrix, int target) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                if (target == num) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 按行二分
+     *
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        for (int[] row : matrix) {
+            int index = binarySearch(row, target);
+            if (index >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int binarySearch(int[] row, int target) {
+        int left = 0, right = row.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (row[mid] == target) {
+                return mid;
+            } else if (row[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Z字查找
+     *
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix3(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] ==target) {
+                return true;
+            }
+            if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
