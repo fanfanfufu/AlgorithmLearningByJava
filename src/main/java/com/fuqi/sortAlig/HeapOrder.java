532fbebf3c3bacf2bbed57f6d235e522e09761c1
fanfanfufu <1127137365@qq.com>
2022-10-13 00:48:45 +0800

    2022-10-12：每日一练
    912：数组排序，考察排序算法

--- src/main/java/com/fuqi/sortAlig/HeapOrder.java
+++ src/main/java/com/fuqi/sortAlig/HeapOrder.java
@@ -7,9 +7,9 @@ package com.fuqi.sortAlig;
  * @Version V1.0
  */
 public class HeapOrder {
-    private static void order(int[] arr){
+    private static void order(int[] arr) {
         // 1.构建大顶堆
-        for (int i = arr.length/2 - 1; i >= 0; i--) {
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
             adjustHeap(arr, i, arr.length);
         }
 
@@ -20,18 +20,20 @@ public class HeapOrder {
         }
     }
 
-    private static void adjustHeap(int[] arr, int i, int len){
+    private static void adjustHeap(int[] arr, int i, int len) {
         int temp = arr[i];
         //从i结点的左子结点开始，也就是2i+1处开始
         for (int j = i * 2 + 1; j < len; j = 2 * j + 1) {
             //如果左子结点小于右子结点，j指向右子结点
-            if (j+1 < len && arr[j] < arr[j+1]){ j++; }
+            if (j + 1 < len && arr[j] < arr[j + 1]) {
+                j++;
+            }
 
             //如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
-            if (arr[j] > temp){
+            if (arr[j] > temp) {
                 arr[i] = arr[j];
                 i = j;
-            }else {
+            } else {
                 break;
             }
         }
@@ -39,16 +41,50 @@ public class HeapOrder {
         arr[i] = temp;
     }
 
-    private static void swap(int[] arr, int a, int b){
+    private static void swap(int[] arr, int a, int b) {
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
     }
 
-    public static void main(String[] args){
-        int[] arr = {4,6,8,5,9};
+    public int[] sortArray(int[] nums) {
+        int len = nums.length;
+        heapify(nums);
+        for (int i = len - 1; i >= 1; ) {
+            swap(nums, 0, i);
+            i--;
+            siftDown(nums, 0, i);
+        }
+
+        return nums;
+    }
+
+    private void heapify(int[] nums) {
+        int len = nums.length;
+        for (int i = (len - 1) / 2; i >= 0; i--) {
+            siftDown(nums, i, len - 1);
+        }
+    }
+
+    private void siftDown(int[] nums, int k, int end) {
+        while (2 * k + 1 <= end) {
+            int j = 2 * k + 1;
+            if (j + 1 <= end && nums[j + 1] > nums[j]) {
+                j++;
+            }
+            if (nums[j] > nums[k]) {
+                swap(nums, j, k);
+            } else {
+                break;
+            }
+            k = j;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 6, 8, 5, 9};
         order(arr);
-        for (int num: arr) {
+        for (int num : arr) {
             System.out.print(num + " ");
         }
     }
