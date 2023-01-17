033a8fe5d241eb495a0eda344b99679764a44491
fanfanfufu <1127137365@qq.com>
2023-01-17 22:35:35 +0800

    2023-01-17：每日一练
    215: 数组中的第K个最大元素

--- src/main/java/com/fuqi/year2022/month01/Solution00215.java
+++ src/main/java/com/fuqi/year2022/month01/Solution00215.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 /**
  * @author FuQi
  * @date 2022/1/22 15:11
- * @description
+ * @description 数组中的第K个最大元素
  */
 public class Solution00215 {
     public static int findKthLargest(int[] nums, int k) {
@@ -36,6 +36,49 @@ public class Solution00215 {
         List<Integer> sortedNums = Arrays.stream(nums).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
 
         return sortedNums.get(k-1);
+    }
+
+    private final static  Random random = new Random(System.currentTimeMillis());
+
+    public int findKthLargest3(int[] nums, int k) {
+        int len = nums.length;
+        int target = len - k;
+        int left = 0, right = len - 1;
+        while (true) {
+            int pivotIndex = partition(nums, left, right);
+            if (pivotIndex == target) {
+                return nums[pivotIndex];
+            } else if (pivotIndex < target) {
+                left = pivotIndex + 1;
+            } else {
+                right = pivotIndex - 1;
+            }
+        }
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int randomIndex = left + random.nextInt(right - left + 1);
+        swap(nums, left, randomIndex);
+        int pivot = nums[left];
+        int le = left + 1;
+        int ge = right;
+        while (true) {
+            while (le <= ge && nums[le] < pivot) le++;
+            while (le <= ge && nums[ge] > pivot) ge--;
+            if (le >= ge) break;
+            swap(nums, le, ge);
+            le++;
+            ge--;
+        }
+        swap(nums, left, ge);
+
+        return ge;
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 
     public static void main(String[] args) {
