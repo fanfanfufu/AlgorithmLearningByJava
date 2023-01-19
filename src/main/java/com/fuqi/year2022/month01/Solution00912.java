7c4267bc8781072d37978cabd69cf4269eff81b3
fanfanfufu <1127137365@qq.com>
2023-01-19 22:30:50 +0800

    2023-01-18：每日一练
    912: 熟悉快速排序

--- src/main/java/com/fuqi/year2022/month01/Solution00912.java
+++ src/main/java/com/fuqi/year2022/month01/Solution00912.java
@@ -1,5 +1,7 @@
 package com.fuqi.year2022.month01;
 
+import java.util.Random;
+
 /**
  * @author FuQi
  * @date 2022/1/23 20:43
@@ -34,20 +36,12 @@ public class Solution00912 {
         while (i < j) {
             // 需要先处理右边的元素
             // 右边的元素大于或等于基准值，则移动指针即可
-            while (i < j && nums[j] >= base) {
-                j--;
-            }
+            while (i < j && nums[j] >= base) j--;
             // 如果左边的元素小于或等于基准值，则只需移动指针即可
-            while (i < j && nums[i] <= base) {
-                i++;
-            }
+            while (i < j && nums[i] <= base) i++;
             // 至此，说明nums[i] > base或者nums[j] < base
             // 因此需要将i,j的元素进行交换
-            if (i < j) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-            }
+            if (i < j) swap(nums, i, j);
         }
         // 循环完成后，将基准值与i,j相等处的元素进行交换
         nums[left] = nums[i];
@@ -56,6 +50,79 @@ public class Solution00912 {
         quickSort(nums, left, i-1);
         // 递归排序基准值右边区域的数据
         quickSort(nums, i+1, right);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private static final int INSERTION_SORT_THRESHOLD = 7;
+
+    private static final Random RANDOM = new Random();
+
+    /**
+     * 双指针快排
+     *
+     * @param nums
+     * @param left
+     * @param right
+     */
+    private void quickSort2(int[] nums, int left, int right) {
+        // 小区间使用插入排序
+        if (right - left <= INSERTION_SORT_THRESHOLD) {
+            insertionSort(nums, left, right);
+            return;
+        }
+
+        int randomIndex = left + RANDOM.nextInt(right - left + 1);
+        swap(nums, randomIndex, left);
+
+        // 循环不变量：
+        // all in [left + 1, lt] < pivot
+        // all in [lt + 1, i) = pivot
+        // all in [gt, right] > pivot
+        int pivot = nums[left];
+        int lt = left;
+        int gt = right + 1;
+
+        int i = left + 1;
+        while (i < gt) {
+            if (nums[i] < pivot) {
+                lt++;
+                swap(nums, i, lt);
+                i++;
+            } else if (nums[i] == pivot) {
+                i++;
+            } else {
+                gt--;
+                swap(nums, i, gt);
+            }
+        }
+        swap(nums, left, lt);
+        // 注意这里，大大减少了两侧分治的区间
+        quickSort2(nums, left, lt - 1);
+        quickSort2(nums, gt, right);
+    }
+
+    /**
+     * 对数组 nums 的子区间 [left, right] 使用插入排序
+     *
+     * @param nums  给定数组
+     * @param left  左边界，能取到
+     * @param right 右边界，能取到
+     */
+    private void insertionSort(int[] nums, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int temp = nums[i];
+            int j = i;
+            while (j > left && nums[j - 1] > temp) {
+                nums[j] = nums[j - 1];
+                j--;
+            }
+            nums[j] = temp;
+        }
     }
 
     public static void main(String[] args) {
