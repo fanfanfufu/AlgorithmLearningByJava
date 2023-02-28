6605f0e3eb5c53c1aec4431c9c9876eb763095c3
fanfanfufu <1127137365@qq.com>
2023-02-28 23:06:17 +0800

    2023-02-28：每日一练
    23：合并K个升序链表复习

--- src/main/java/com/fuqi/year2022/month05/Solution00023.java
+++ src/main/java/com/fuqi/year2022/month05/Solution00023.java
@@ -13,6 +13,14 @@ public class Solution00023 {
         return merge(lists, 0, lists.length - 1);
     }
 
+    /**
+     * 对链表进行分治
+     *
+     * @param lists
+     * @param left
+     * @param right
+     * @return
+     */
     private ListNode merge(ListNode[] lists, int left, int right) {
         if (left == right) return lists[left];
         int mid = left + (right - left) / 2;
@@ -31,5 +39,45 @@ public class Solution00023 {
             l2.next = mergeTwoLists(l1,l2.next);
             return l2;
         }
+    }
+
+    /**
+     * 方法一：遍历合并
+     *
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists1(ListNode[] lists) {
+        ListNode ans = null;
+        for (int i = 0; i < lists.length; i++) {
+            ans = mergeTwoLists2(ans, lists[i]);
+        }
+
+        return ans;
+    }
+
+    /**
+     * 以递归的方式实现
+     *
+     * @param list1
+     * @param list2
+     * @return
+     */
+    private ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode head;
+        if (list1.val < list2.val) {
+            head = list1;
+            list1 = list1.next;
+        } else {
+            head = list2;
+            list2 = list2.next;
+        }
+
+        head.next = mergeTwoLists2(list1, list2);
+
+        return head;
     }
 }
