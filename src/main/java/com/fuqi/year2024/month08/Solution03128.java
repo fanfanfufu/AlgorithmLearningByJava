a5e801bd7675fbc52c0738f6dd835d046a66c92d
fanfanfufu <1127137365@qq.com>
2024-08-02 23:54:54 +0800

    2024-08-02: 每日一练-3128

--- src/main/java/com/fuqi/year2024/month08/Solution03128.java
+++ src/main/java/com/fuqi/year2024/month08/Solution03128.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 public class Solution03128 {
     public long numberOfRightTriangles(int[][] grid) {
         int n = grid.length, m = grid[0].length;
+        // 统计每一列有多少个1
         int[] col = new int[m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -19,6 +20,7 @@ public class Solution03128 {
         }
         long res = 0;
         for (int i = 0; i < n; i++) {
+            // row表示每一行有多少个1
             int row = Arrays.stream(grid[i]).sum();
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
