a200719d28332af6f0202c167023638f17e25e72
fanfanfufu <1127137365@qq.com>
2024-08-11 17:32:06 +0800

    2024-08-11: 每日一练-1035

---
+++ src/main/java/com/fuqi/year2024/month08/Solution01035.java
@@ -0,0 +1,25 @@
+package com.fuqi.year2024.month08;
+
+/**
+ * @author fanfanfufu
+ * @version 1.0
+ * @date 2024/8/11 16:33
+ * @description
+ */
+public class Solution01035 {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int[][] dp = new int[m+1][n+1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (nums1[i-1] == nums2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
